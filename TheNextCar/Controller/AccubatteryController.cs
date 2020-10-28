8a0fb62c3a8faaeba2854019947483ad2196ccc0
Muhammad Kholif Arrahman <Muhammad.2799@students.amikom.ac.id>
2020-10-28 20:10:30 +0700

    Add project files.

---
+++ TheNextCar/Controller/AccubatteryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheNextCar.Model;
+
+namespace TheNextCar.Controller
+{
+    class AccubatteryController
+    {
+        private Accubattery accubattery;
+        private OnPowerChanged callbackOnPowerChanged;
+
+        public AccubatteryController(OnPowerChanged callbackOnPowerChanged)
+        {
+            this.callbackOnPowerChanged = callbackOnPowerChanged;
+            this.accubattery = new Accubattery(12);
+        }
+
+        public bool accubatteryIsOn()
+        {
+            return this.accubattery.isOn();
+        }
+        public void turnOn()
+        {
+            this.accubattery.turnOn();
+            this.callbackOnPowerChanged.OnPowerChangedStatus("ON", "power is on");
+
+        }
+        public void turnOff()
+        {
+            this.accubattery.turnOff();
+            this.callbackOnPowerChanged.OnPowerChangedStatus("OFF", "power is off");
+        }
+    }
+
+}
+
+interface OnPowerChanged
+{
+    void OnPowerChangedStatus(string value, string message);
+}
---
+++ TheNextCar/Controller/Car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TheNextCar.Controller
+{
+    class Car
+    {
+        private DoorController doorController;
+        private AccubatteryController accubatteryController;
+        private OnCarEngineStateChanged callback;
+
+        public Car(DoorController doorController, AccubatteryController accuBatteryController, OnCarEngineStateChanged callback)
+        {
+            this.doorController = doorController;
+            this.accubatteryController = accuBatteryController;
+            this.callback = callback;
+        }
+        private bool doorIsClosed()
+        {
+            return this.doorController.isClose();
+        }
+        private bool doorIsLocked()
+        {
+            return this.doorController.isLocked();
+        }
+        private bool powerIsReady()
+        {
+            return this.accubatteryController.accubatteryIsOn();
+        }
+        public void startEngine()
+        {
+            if (!doorIsClosed())
+            {
+                this.callback.OnCarEngineStateChanged("STOPED", "close the door");
+                return;
+            }
+            if (!doorIsLocked())
+            {
+                this.callback.OnCarEngineStateChanged("STOPED", "Lock the door");
+                return;
+            }
+            if (!powerIsReady())
+            {
+                this.callback.OnCarEngineStateChanged("STOPED", "no power available");
+                return;
+            }
+            this.callback.OnCarEngineStateChanged("STARTED", "Engine Started");
+        }
+        public void toggleTheLockDoorButton()
+        {
+            if (!doorIsLocked())
+            {
+                this.doorController.activateLock();
+            }
+            else
+            {
+                this.doorController.unlock();
+            }
+        }
+        public void toggleTheOpenDoorButton()
+        {
+            if (!doorIsClosed())
+            {
+                this.doorController.close();
+            }
+            else
+            {
+                this.doorController.open();
+            }
+        }
+        public void togglePowerButton()
+        {
+            if (!powerIsReady())
+            {
+                this.accubatteryController.turnOn();
+            }
+            else
+            {
+                this.accubatteryController.turnOff();
+            }
+        }
+    }
+
+    interface OnCarEngineStateChanged
+    {
+        void OnCarEngineStateChanged(string value, string message);
+    }
+}
